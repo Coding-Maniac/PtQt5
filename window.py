1e1a3925146e24480b723a038cc8043e7c9c5581
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2019-06-14 10:32:44 +0530

    Update window.py

--- window.py
+++ window.py
@@ -1,1 +1,23 @@
 #This code helps in creating a dialog box with an icon
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication,QMainWindow
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = "My Window"
+        self.top =100
+        self.left = 100
+        self.width =680
+        self.height = 500
+        self.InitWindow()
+        self.setWindowIcon(QtGui.QIcon("mass.png"))
+
+    def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top,self.left,self.width,self.height)
+        self.show()
+App = QApplication(sys.argv)
+window =Window()
+sys.exit(App.exec())
