db4e247a9c437bcbf056ac2ef6070effcb088f8e
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2019-06-16 23:55:50 +0530

    Update DateTime.py

--- DateTime.py
+++ DateTime.py
@@ -2,3 +2,10 @@
 from PyQt5.QtCore import QDateTime,QTime,Qt,QDate
 
 datetime = QDateTime.currentDateTime()
+
+print("_______________________________")
+print(datetime.toString())
+print(datetime.toString(Qt.ISODate))
+print(datetime.toString(Qt.DefaultLocaleLongDate))
+print("_______________________________")
+
